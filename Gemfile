fe43895987c86011383f4c801e7307aba7ee53d5
Viatcheslav Fedorov <fedorov555@gmail.com>
2011-09-16 19:34:16 +0400

    Users is done

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '3.1.0'
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
+gem 'gravatar_image_tag'
 
 
 # Gems used only for assets and not required
@@ -27,5 +28,6 @@ end
 group :test do
   gem 'rspec-rails', '2.6.1'
   gem 'webrat', '0.7.1'
-end
+	gem 'factory_girl_rails' 
+	end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,5 +8,21 @@ describe UsersController do
       response.should be_success
     end
   end
+	
+	describe "GET 'show'" do
 
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+  end
 end
