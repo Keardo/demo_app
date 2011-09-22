4c7a05fba77d9a2424f9792c648748ee0936ec15
Viatcheslav Fedorov <fedorov555@gmail.com>
2011-09-22 22:00:45 +0400

    to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ DemoApp::Application.routes.draw do
   resources :users
 
 	match '/signup',  :to => 'users#new'
+  resources :sessions, :only => [:new, :create, :destroy]
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
